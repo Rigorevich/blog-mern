57b593e191bab93ece7afdab96b48bf1d40b2f76
Rigorevich <rigorevich268@gmail.com>
2022-07-15 13:17:42 +0300

    create auth validator

--- index.js
+++ index.js
@@ -14,18 +14,8 @@ app.get('/', (req, res) => {
     res.send('Hi!');
 });
 
-app.post('/auth/login', (req, res) => {
-    console.log(req.body);
+app.post('/auth/register', (req, res) => {
 
-    const token = jwt.sign({
-        email: req.body.email,
-        fullName: 'Вася Пупкин'
-    }, 'secret123');
-
-    res.json({
-        success: true,
-        token
-    });
 });
 
 app.listen(4445, (err) => {
