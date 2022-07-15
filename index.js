3c30cce281810082edeacbd3c399fe40443aad75
Rigorevich <rigorevich268@gmail.com>
2022-07-15 16:38:50 +0300

    create registration

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import {validationResult} from "express-validator";
 
 import {registerValidation} from "./validations/auth.js";
 
-mongoose.connect(
-    'mongodb+srv://admin:wwwwww@cluster0.dsdyo.mongodb.net/?retryWrites=true&w=majority'
-).then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
+import UserModel from './models/User.js'
+
+mongoose.connect('mongodb+srv://admin:wwwwww@cluster0.dsdyo.mongodb.net/blog?retryWrites=true&w=majority')
+    .then(() => console.log('DB ok'))
+    .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', registerValidation, (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
+app.post('/auth/login',async (req, res) => {
+    try {
+        const user = new UserModel({
+            email: req.body.email,
+        });
+
+        if (!user) {
+            return req.status(404).json({
+                message: 'Пользователь не найден.'
+            });
+        }
+    } catch (err) {
+
     }
-    res.json({
-        success: true
-    });
+})
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt);
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign({
+            _id: user._id
+        }, 'secret123', {
+            expiresIn: '30d'
+        }
+        );
+
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            userData,
+            token
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось зарегистрировать.'
+        });
+    }
 });
 
 app.listen(4445, (err) => {
