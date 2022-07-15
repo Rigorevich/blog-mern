5b50b787aefbef9c84520af188f07abfe012c64b
Rigorevich <rigorevich268@gmail.com>
2022-07-15 13:10:50 +0300

    create model of User

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose.connect(
+    'mongodb+srv://admin:wwwwww@cluster0.dsdyo.mongodb.net/?retryWrites=true&w=majority'
+).then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hi!');
 });
 
 app.post('/auth/login', (req, res) => {
+    console.log(req.body);
+
+    const token = jwt.sign({
+        email: req.body.email,
+        fullName: 'Вася Пупкин'
+    }, 'secret123');
+
     res.json({
-        success: true
+        success: true,
+        token
     });
 });
 
-app.listen(444, (err) => {
+app.listen(4445, (err) => {
     if (err) console.log(err);
     console.log('Server has been started...');
 });
