21f13cd74f17fde350ee3b544315a73374f154f0
Rigorevich <rigorevich268@gmail.com>
2022-07-15 17:10:11 +0300

    create log in

--- index.js
+++ index.js
@@ -18,17 +18,43 @@ app.use(express.json());
 
 app.post('/auth/login',async (req, res) => {
     try {
-        const user = new UserModel({
+        const user = await UserModel.findOne({
             email: req.body.email,
-        });
+        })
 
         if (!user) {
-            return req.status(404).json({
+            return res.status(404).json({
                 message: 'Пользователь не найден.'
             });
         }
-    } catch (err) {
 
+        const isValidPassword = await bcrypt.compare(req.body.password, user._doc.passwordHash);
+        if (!isValidPassword) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль'
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id
+            },
+            'secret123',
+            {
+                expiresIn: '30d'
+            }
+        );
+        const { passwordHash, ...userData } = user._doc;
+
+        res.json({
+            ...userData,
+            token
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось авторизоваться.'
+        });
     }
 })
 
@@ -62,7 +88,7 @@ app.post('/auth/register', registerValidation, async (req, res) => {
         const { passwordHash, ...userData } = user._doc;
 
         res.json({
-            userData,
+            ...userData,
             token
         });
     } catch (err) {
@@ -72,6 +98,14 @@ app.post('/auth/register', registerValidation, async (req, res) => {
         });
     }
 });
+
+app.get('/auth/me', (req, res) => {
+    try {
+
+    } catch (err) {
+
+    }
+})
 
 app.listen(4445, (err) => {
     if (err) console.log(err);
